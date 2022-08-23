adf4a3e279109204a95c0c504a7030e80a8d23f9
ano201 <ahmu689@gmail.com>
2022-08-24 05:43:24 +0600

    text Animation added and wish added

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const candle = document.getElementsByClassName('candle');
+const candle = () => {
+  const candle = document.getElementsByClassName('candle');
+  setTimeout(() => {
+    candle[0].style.top = "289px";
+    wishes();
+  }, 4500);
+}
 
-setTimeout(() => {
-  candle[0].style.top = "289px";
-}, 4500);
+const wishes = () => {
+  const text = document.querySelectorAll('h2');
+  let index = 0;
+  const interval = setInterval(() => {
+    if (index <= 3) {
+      text[index].style.margin = 0;
+      index++;
+    }
+    else {
+      clearInterval(interval);
+    }
+  }, 1500)
+};
