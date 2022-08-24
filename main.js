8d2ce0d510a47d5e13a4b3f68ed3cf7197fb8f3a
ano201 <ahmu689@gmail.com>
2022-08-24 17:00:31 +0600

    transition added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const candle = () => {
+  confetti()
   const candle = document.getElementsByClassName('candle');
   setTimeout(() => {
     candle[0].style.top = "289px";
@@ -8,6 +9,8 @@ const candle = () => {
 
 const wishes = () => {
   const text = document.querySelectorAll('h2');
+  const lastText = document.getElementsByClassName('text');
+  lastText[1].style.opacity = 1;
   let index = 0;
   const interval = setInterval(() => {
     if (index <= 3) {
@@ -19,3 +22,17 @@ const wishes = () => {
     }
   }, 1500)
 };
+
+confetti = () => {
+  const body = document.body;
+  const script = document.createElement("script");
+  script.setAttribute("src", "confetti.js")
+  const script2 = document.createElement("script");
+  script2.innerHTML = `setTimeout(() => {
+startConfetti();
+}, 5000);
+setTimeout(() => {
+  stopConfetti();
+}, 20000);`
+  body.append(script, script2)
+}
